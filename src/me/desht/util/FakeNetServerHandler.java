ff07b4efe9bb9a613d67ae1ec7b3319f4e0138e9
Des Herriott <des.herriott@gmail.com>
2011-09-07 17:35:00 +0100

    Initial check-in
    Change-Id: I221fd1a1b376ef4c3489e3acdbb4315d4c1f2f8b

---
+++ src/me/desht/util/FakeNetServerHandler.java
@@ -0,0 +1,32 @@
+package me.desht.util;
+
+import net.minecraft.server.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.NetServerHandler;
+import net.minecraft.server.NetworkManager;
+import net.minecraft.server.Packet;
+import net.minecraft.server.Packet10Flying;
+import net.minecraft.server.Packet3Chat;
+
+public class FakeNetServerHandler extends NetServerHandler {
+	public FakeNetServerHandler(MinecraftServer minecraftserver, NetworkManager networkmanager, EntityPlayer entityplayer)
+	{
+		super(minecraftserver, networkmanager, entityplayer);
+	}
+
+	//In space, no one can hear you scream.
+	@Override
+	public void sendPacket(Packet packet)
+	{
+		//For real, don't say ANYTHING to the fake player.
+		if (!(packet instanceof Packet3Chat))
+			super.sendPacket(packet);
+	}
+
+	//I believe I can fly!
+	@Override
+	public void a(Packet10Flying packet10flying) {
+		//Don't do anything here.  ESPECIALLY don't call super.a; you'll get kicked.
+	}
+
+}
