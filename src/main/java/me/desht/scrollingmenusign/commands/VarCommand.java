53533094de50f5168915fed5f0d70fc5a8a2f1f7
Des Herriott <des.herriott@gmail.com>
2012-11-23 11:29:10 +0000

    /sms var command now uses getOptions() API, much cleaner

--- src/main/java/me/desht/scrollingmenusign/commands/VarCommand.java
+++ src/main/java/me/desht/scrollingmenusign/commands/VarCommand.java
@@ -20,67 +20,59 @@ public class VarCommand extends AbstractCommand {
 				"/sms var [-q] -d [<player>.]<variable>",
 				"/sms var [-q] -i [<player>.]<variable> [<amount>]",
 		});
+		setOptions(new String[] { "q", "l", "d", "i" });
 		setQuotedArgs(true);
 	}
 
 	@Override
 	public boolean execute(Plugin plugin, CommandSender sender, String[] args) {
 
-		if (args[0].startsWith("-l")) {
+		boolean quiet = getBooleanOption("q");
+
+		if (getBooleanOption("l")) {
 			// list all variables for the player
 
-			if (args.length == 1) notFromConsole(sender);
+			if (args.length == 0) notFromConsole(sender);
 
-			String playerName = args.length > 1 ? args[1] : sender.getName();
+			String target = args.length >= 1 ? args[0] : sender.getName();
 
-			SMSVariables vars = SMSVariables.getVariables(playerName, false);
+			SMSVariables vars = SMSVariables.getVariables(target, false);
 			MessagePager pager = MessagePager.getPager(sender).clear();
 			for (String key : vars.getVariables()) {
 				pager.add(key + " = '&e" + vars.get(key) + "&-'");	
 			}
 			pager.showPage();
-
-		} else if (args.length == 1) {
-			// show the given variable for the player
-			String varSpec = args[0];
-			String val = SMSVariables.get(sender, varSpec);
-			if (val != null) {
-				String def = SMSVariables.isSet(sender, varSpec) ? "" : " &6(default)";
-				MiscUtil.statusMessage(sender, varSpec + " = '&e" + val + "&-'" + def);
-			} else {
-				throw new SMSException("No such variable: " + varSpec);
+			return true;
+		} 
+		
+		String varSpec = args[0];
+		
+		if (getBooleanOption("d")) {
+			// delete the variable
+			SMSVariables.set(sender, varSpec, null);
+			if (!quiet)
+				MiscUtil.statusMessage(sender, "Deleted variable &f" + varSpec + "&-.");
+		} else if (getBooleanOption("i")) {
+			// add a numeric quantity to the variable
+			try {
+				int val = Integer.parseInt(SMSVariables.get(sender, varSpec));
+				int incr = args.length >= 2 ? Integer.parseInt(args[1]) : 1;
+				SMSVariables.set(sender, varSpec, Integer.toString(val + incr));
+				if (!quiet)
+					MiscUtil.statusMessage(sender, varSpec + " = '&e" + (val+incr) + "&-'");
+			} catch (NumberFormatException e) {
+				throw new SMSException(e.getMessage() + ": not a number");
 			}
 		} else if (args.length >= 2) {
-
-			int n = 0;
-			boolean quiet = false;
-			if (args[0].startsWith("-q")) {
-				quiet = true;
-				n = 1;
-			}
-
-			if (args[n].startsWith("-d")) {
-				// delete the variable
-				SMSVariables.set(sender, args[n+1], null);
-				if (!quiet)
-					MiscUtil.statusMessage(sender, "Deleted variable &f" + args[n+1] + "&-.");
-			} else if (args[n].startsWith("-i")) {
-				// increment by the given amount (if possible)
-				try {
-					int val = Integer.parseInt(SMSVariables.get(sender, args[n+1]));
-					int incr = args.length > n+2 ? Integer.parseInt(args[n+2]) : 1;
-					SMSVariables.set(sender, args[n+1], Integer.toString(val + incr));
-					if (!quiet)
-						MiscUtil.statusMessage(sender, args[n+1] + " = '&e" + (val+incr) + "&-'");
-				} catch (NumberFormatException e) {
-					throw new SMSException(e.getMessage() + ": not a number");
-				}
-			} else {
-				// just set the variable
-				SMSVariables.set(sender, args[n], args[n+1]);;
-				if (!quiet)
-					MiscUtil.statusMessage(sender, args[n] + " = '&e" + args[n+1] + "&-'");
-			}
+			// set the variable
+			SMSVariables.set(sender, varSpec, args[1]);
+			if (!quiet)
+				MiscUtil.statusMessage(sender, varSpec + " = '&e" + args[1] + "&-'");
+		} else {
+			// just display the variable
+			String def = SMSVariables.isSet(sender, varSpec) ? "" : " &6(default)";
+			String val = SMSVariables.get(sender, varSpec);
+			MiscUtil.statusMessage(sender, varSpec + " = '&e" + val + "&-'" + def);
 		}
 
 		return true;
