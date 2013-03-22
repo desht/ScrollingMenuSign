a7f148ff1964fb1357e65edea9ad8ff1ed12e1a7
Des Herriott <des.herriott@gmail.com>
2013-03-22 08:57:22 +0000

    /sms font now lists fonts comma-separated perms fix in plugin.yml Build with dhutils 2.2.1

--- src/main/java/me/desht/scrollingmenusign/commands/FontCommand.java
+++ src/main/java/me/desht/scrollingmenusign/commands/FontCommand.java
@@ -3,40 +3,45 @@ package me.desht.scrollingmenusign.commands;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
-
 import me.desht.dhutils.MessagePager;
 import me.desht.dhutils.commands.AbstractCommand;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
 
 public class FontCommand extends AbstractCommand {
 
 	public FontCommand() {
 		super("sms f", 0, 1);
-		setPermissionNode("sms.commands.font");
+		setPermissionNode("scrollingmenusign.commands.font");
 		setUsage("/sms font");
 	}
 
 	@Override
 	public boolean execute(Plugin plugin, CommandSender sender, String[] args) {
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-	    Font[] fonts = ge.getAllFonts();
+		Font[] fonts = ge.getAllFonts();
 
-	    MessagePager pager = MessagePager.getPager(sender).clear();
-	    
-	    int matched = 0;
-	    for (int i = 0; i < fonts.length; i++) {
-	    	String fn = fonts[i].getName();
-	    	if (args.length >= 1 && !fn.toLowerCase().contains(args[0].toLowerCase())) {
-	    		continue;
-	    	}
-	    	pager.add(MessagePager.BULLET + ChatColor.WHITE + fn);
-	    	matched++;
-	    }
-	    pager.add(matched + " fonts matched (of " + fonts.length + " total)");
-	    pager.showPage();
-	    
+		MessagePager pager = MessagePager.getPager(sender).clear();
+
+		int matched = 0;
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < fonts.length; i++) {
+			String fn = fonts[i].getName();
+			if (args.length >= 1 && !fn.toLowerCase().contains(args[0].toLowerCase())) {
+				continue;
+			}
+			if (sb.length() + fn.length() > 60) {
+				pager.add(sb.toString());
+				sb.setLength(0);
+			}
+			sb.append(fn + ", ");
+			matched++;
+		}
+		pager.add(sb.toString());
+		pager.add(matched + " fonts matched (of " + fonts.length + " total)");
+		pager.showPage();
+
 		return true;
 	}
 
