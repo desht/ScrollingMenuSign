a957ba692a82071ea5f3c2dd4c007ea5457572e0
Des Herriott <des.herriott@gmail.com>
2014-05-28 15:02:20 +0100

    Added item usage limit unit testing (thanks Mockito)

--- src/test/java/me/desht/scrollingmenusign/SMSMenuItemTest.java
+++ src/test/java/me/desht/scrollingmenusign/SMSMenuItemTest.java
@@ -11,6 +11,10 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.junit.Test;
 
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
 public class SMSMenuItemTest {
     private final SMSMenu testMenu;
 
@@ -56,5 +60,34 @@ public class SMSMenuItemTest {
         assertEquals("Icon is diamond", item2.getIcon().getType(), Material.DIAMOND);
         assertEquals("Lore has 3 lines", item2.getLore().length, 3);
         assertEquals("Permission node is correct", item2.getPermissionNode(), "some.permission.node");
+    }
+
+    @Test
+    public void testItemUseLimits() {
+        UUID mockUUID1 = UUID.randomUUID();
+        UUID mockUUID2 = UUID.randomUUID();
+        Player mockedPlayer = mock(Player.class);
+        when(mockedPlayer.getUniqueId()).thenReturn(mockUUID1);
+        Player mockedPlayer2 = mock(Player.class);
+        when(mockedPlayer2.getUniqueId()).thenReturn(mockUUID2);
+
+        SMSMenuItem item1 = new SMSMenuItem.Builder(testMenu, "Use Limits")
+                .withCommand("\\hello!")
+                .build();
+
+        item1.getUseLimits().setUses(5);
+        item1.getUseLimits().use(mockedPlayer);
+        item1.getUseLimits().use(mockedPlayer2);
+        item1.getUseLimits().use(mockedPlayer2);
+        assertEquals("Per-player use limits 1", item1.getUseLimits().getRemainingUses(mockedPlayer), 4);
+        assertEquals("Per-player use limits 2", item1.getUseLimits().getRemainingUses(mockedPlayer2), 3);
+
+        item1.getUseLimits().setGlobalUses(10);
+        item1.getUseLimits().use(mockedPlayer);
+        item1.getUseLimits().use(mockedPlayer);
+        item1.getUseLimits().use(mockedPlayer);
+        item1.getUseLimits().use(mockedPlayer2);
+        assertEquals("Global use limits 1", item1.getUseLimits().getRemainingUses(mockedPlayer), 6);
+        assertEquals("Global use limits 2", item1.getUseLimits().getRemainingUses(mockedPlayer2), 6);
     }
 }
