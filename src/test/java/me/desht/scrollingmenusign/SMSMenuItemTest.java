4f79bcc45cf8691a20d96c5643c754ef2bf816d4
Des Herriott <des.herriott@gmail.com>
2014-05-28 15:15:09 +0100

    more unit testing

--- src/test/java/me/desht/scrollingmenusign/SMSMenuItemTest.java
+++ src/test/java/me/desht/scrollingmenusign/SMSMenuItemTest.java
@@ -7,15 +7,22 @@ import static org.junit.Assert.assertFalse;
 import me.desht.dhutils.LogUtils;
 
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.MemoryConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.material.Dye;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import static org.mockito.Mockito.*;
 
 public class SMSMenuItemTest {
+    private final UUID mockUUID1 = UUID.randomUUID();
+    private final UUID mockUUID2 = UUID.randomUUID();
     private final SMSMenu testMenu;
 
     public SMSMenuItemTest() {
@@ -63,9 +70,33 @@ public class SMSMenuItemTest {
     }
 
     @Test
+    public void testDeserialization() {
+        ConfigurationSection node = new MemoryConfiguration();
+        node.set("menu", testMenu.getName());
+        node.set("label", "Label 1 2 3");
+        node.set("command", "\\foo!");
+        node.set("message", "a message for you");
+        node.set("icon", "INK_SACK:WHITE");
+        node.set("lore", Arrays.asList("line 1", "line 2"));
+        node.set("usesRemaining.limits.globalMax", 10);
+        node.set("usesRemaining.limits.globalRemaining", 8);
+        SMSMenuItem item1 = new SMSMenuItem(testMenu, node);
+
+        assertEquals("Label", item1.getLabel(), "Label 1 2 3");
+        assertEquals("Command", item1.getCommand(), "\\foo!");
+        assertEquals("Message", item1.getMessage(), "a message for you");
+        assertEquals("Alt Command", item1.getAltCommand(), "");
+        Player mockedPlayer = mock(Player.class);
+        when(mockedPlayer.getUniqueId()).thenReturn(mockUUID1);
+        assertEquals("Usage", item1.getUseLimits().getRemainingUses(mockedPlayer), 8);
+        assertEquals("Icon mat", item1.getIcon().getType(), Material.INK_SACK);
+        Dye dye = (Dye) item1.getIcon().getData();
+        assertEquals("Icon data", dye.getColor(), DyeColor.WHITE);
+        assertEquals("Lore", item1.getLore()[1], "line 2");
+    }
+
+    @Test
     public void testItemUseLimits() {
-        UUID mockUUID1 = UUID.randomUUID();
-        UUID mockUUID2 = UUID.randomUUID();
         Player mockedPlayer = mock(Player.class);
         when(mockedPlayer.getUniqueId()).thenReturn(mockUUID1);
         Player mockedPlayer2 = mock(Player.class);
